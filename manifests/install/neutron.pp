931a75c7f1934606b2c8feca52549a83d266c7db
Luiz Rogerio Carvalho <luizrogeriocarvalho@gmail.com>
2018-07-09 21:04:01 -0300

    Corrected template for neutron.

--- manifests/install/neutron.pp
+++ manifests/install/neutron.pp
@@ -13,13 +13,23 @@ define ostack_controller::install::neutron (
      $admindbpass = 'keatomos3',
      $memcache_host = 'memcache',
      $controller_host = 'controller',
+     $mq_proto = 'rabbit',
+     $mq_user  = 'openstack',
+     $mq_pass  = 'raatomos3',
+     $mq_host  = 'rabbitmq',
      $ostack_region       = 'RegionOne',
      $bstp_adm_port       = '35357',
      $bstp_int_port       = '5000',
      $bstp_pub_port       = '5000',
-     $glance_adm_port       = '9292',
-     $glance_int_port       = '9292',
-     $glance_pub_port       = '9292',
+     $nova_adm_port       = '8774/v2.1',
+     $nova_int_port       = '8774/v2.1',
+     $nova_pub_port       = '8774/v2.1',
+     $placem_adm_port     = '8778',
+     $placem_int_port     = '8778',
+     $placem_pub_port     = '8778',
+     $glance_adm_port     = '9292',
+     $glance_int_port     = '9292',
+     $glance_pub_port     = '9292',
      $neutron_adm_port       = '9696',
      $neutron_int_port       = '9696',
      $neutron_pub_port       = '9696',
@@ -46,39 +56,48 @@ define ostack_controller::install::neutron (
 
    # File configuration
    # We only manage those which need modification
+   file { '/etc/neutron/plugins/ml2/':
+      ensure  => directory,
+   }
    file { 'neutron.conf':
       name    => '/etc/neutron/neutron.conf',
       ensure  => present,
+      require => File['/etc/neutron/plugins/ml2/'],
       content => template('ostack_controller/neutron/neutron.conf.erb'),
       notify  => Exec['neutron-server-restart'],
    }
    file { 'linuxbridge_agent.ini':
       name    => '/etc/neutron/plugins/ml2/linuxbridge_agent.ini',
       ensure  => present,
+      require => File['/etc/neutron/plugins/ml2/'],
       content => template('ostack_controller/neutron/plugins/ml2/linuxbridge_agent.ini.erb'),
       notify  => Exec['neutron-linuxbridge-restart'],
    }
    file { 'ml2_conf.ini':
       name    => '/etc/neutron/plugins/ml2/ml2_conf.ini',
       ensure  => present,
+      require => File['/etc/neutron/plugins/ml2/'],
       content => template('ostack_controller/neutron/plugins/ml2/ml2_conf.ini.erb'),
       notify  => Exec['neutron-linuxbridge-restart'],
    }
    file { 'dhcp_agent.ini':
       name    => '/etc/neutron/dhcp_agent.ini',
       ensure  => present,
+      require => File['/etc/neutron/plugins/ml2/'],
       content => template('ostack_controller/neutron/dhcp_agent.ini.erb'),
       notify  => Exec['neutron-dhcp-restart'],
    }
    file { 'metadata_agent.ini':
       name    => '/etc/neutron/metadata_agent.ini',
       ensure  => present,
+      require => File['/etc/neutron/plugins/ml2/'],
       content => template('ostack_controller/neutron/metadata_agent.ini.erb'),
       notify  => Exec['neutron-metadata-restart'],
    }
    file { 'l3_agent.ini':
       name    => '/etc/neutron/l3_agent.ini',
       ensure  => present,
+      require => File['/etc/neutron/plugins/ml2/'],
       content => template('ostack_controller/neutron/l3_agent.ini.erb'),
       notify  => Exec['neutron-l3-restart'],
    }
@@ -108,7 +127,8 @@ define ostack_controller::install::neutron (
       require     => Exec['NeutronUserCreation'],
       refreshonly => true,
       command     => "openstack role add --project service --user $neutronuser admin",
-   } network service creation
+   } 
+   # network service creation
    exec { 'NetworkServiceCreation':
       path        => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
       environment => $admin_env,
